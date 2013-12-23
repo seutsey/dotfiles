1254dab86817960c140261ed5bdae9728e30e790
Chris Meadows <meadoch1@gmail.com>
2013-12-23 15:36:28 -0600

    Updated to fix rvm

--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 # This should be run from ~/.dotfiles. YOU fix the hardcodes if you don't like 'em.
 cd
 ln -s ~/.dotfiles/bash_completions .bash_completions
+ln -s ~/.dotfiles/dotbash_functions .bash_functions
 ln -s ~/.dotfiles/dotaliases .aliases
 #ln -s ~/.dotfiles/dotemacs.el .emacs
 ln -s ~/.dotfiles/dotvimrc .vimrc
 ln -s ~/.dotfiles/dotirbrc .irbrc
+ln -s ~/.dotfiles/dotbash_profile .bash_profile
+ln -s ~/.dotfiles/dotbashrc .bashrc
 ln -s ~/.dotfiles/dotprofile .profile
+ln -s ~/.dotfiles/dottmux.conf .tmux.conf
+ln -s ~/.dotfiles/dotemacstiny.el .emacstiny
 ln -s ~/.dotfiles/git-completion.bash .git-completion.bash
 cd ~/.dotfiles
